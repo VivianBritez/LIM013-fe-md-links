f2fe230f5c4c3f3784bbd809d2e4edb6da9faed0
vivianBritez <vivianbritez91@gmail.com>
2020-12-05 22:49:01 -0500

    process

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,9 @@
 const chalk = require('chalk');
 const api = require('./mdLinks.js');
 const opt = require('./stats.js');
+const [, , ...args] = process.argv;
+
+console.log("hello soy cli", ...args);
 
 // CLI
 const cli = (route, arg1, arg2) => {
@@ -23,15 +26,15 @@ const cli = (route, arg1, arg2) => {
       if (arg1 === '--validate' && arg2 === undefined) {
         response.forEach((element) => {
           if (element.statusText !== 'OK') {
-            result += `\n${chalk.gray(element.path)} ${chalk.yellow(element.href)} ${chalk.red(element.status)} ${chalk.red(element.statusText)} ${chalk.white(element.text)} ✘`;
+            result += `\n${chalk.white(element.path)} ${chalk.yellow(element.href)} ${chalk.red(element.status)} ${chalk.red(element.statusText)} ${chalk.white(element.text)} ✘`;
           } else {
-            result += `\n${chalk.gray(element.path)} ${chalk.yellow(element.href)} ${chalk.green(element.status)} ${chalk.green(element.statusText)} ${chalk.white(element.text)} ✔`;
+            result += `\n${chalk.white(element.path)} ${chalk.yellow(element.href)} ${chalk.green(element.status)} ${chalk.green(element.statusText)} ${chalk.white(element.text)} ✔`;
           }
         });
       }
       if (arg1 === undefined && arg2 === undefined) {
         response.forEach((element) => {
-          result += `\n${chalk.gray(element.path)} ${chalk.yellow(element.href)} ${chalk.white(element.text)}`;
+          result += `\n${chalk.white(element.path)} ${chalk.yellow(element.href)} ${chalk.white(element.text)}`;
         });
       }
       if (arg1 !== '--stats' && arg1 !== '--validate' && arg1 !== undefined) {
@@ -42,36 +45,42 @@ const cli = (route, arg1, arg2) => {
     .catch(() => chalk.red('Invalid path'));
 };
 // Opcion de help
-const helpOption = `
+const help = `
   ${chalk.yellow.bold`  mdLinks 1.0.0\n`}
   ${chalk.green.bold`  USAGE`}
-  ${chalk.white`    mdLinks `}${chalk.gray`<path>`} ${chalk.yellow`[options]\n`}
+  ${chalk.white`    mdLinks `}${chalk.white`<path>`} ${chalk.yellow`[options]\n`}
   ${chalk.green.bold`  PATH`}
   ${chalk.white`    Is a absolute o relative path of file or directory.\n`}
   ${chalk.green.bold`  OPTIONS`}
-  ${chalk.gray`    -h, --help           `} Display help.
-  ${chalk.gray`    -V, --version        `} Display version.
+  ${chalk.gray`    -h, --help           `} help.
+  ${chalk.gray`    -V, --version        `} version.
   ${chalk.gray`    --stats              `} Basic stadistics on link.
   ${chalk.gray`    --validate           `} Link validation.
-  ${chalk.gray`    --stats --validate   `} Statistics that require the validation results.`;
+  ${chalk.gray`    --stats --validate   `} Stats and validate.
+  ${chalk.gray`    -c, --contact        `} Vivian Britez (vivianbritez91@gmail.com).
+  `;
 
-const [,, ...args] = process.argv;
+
+
 if (args.length < 4) {
   if (args[0] === '--help' || args[0] === '-h') {
-    console.log(helpOption);
+    console.log(help);
   }
   if (args[0] === undefined) {
-    console.log(helpOption);
+    console.log(help);
   }
   if (args[0] === '-V' || args[0] === '--version') {
     console.log('1.0.0');
-  } else {
+  } 
+  if (args[0] === '-c' || args[0] === '--contact') {
+    console.log('Vivian Britez (vivianbritez91@gmail.com)');
+  }else {
     cli(args[0], args[1], args[2])
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   }
 } else {
-  console.log(helpOption);
+  console.log(help);
 }
 
 module.exports = { cli };
