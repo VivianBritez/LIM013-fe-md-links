7631d17dbf06526edd337ddcea22dfc65ca73102
vivianBritez <vivianbritez91@gmail.com>
2020-12-21 18:39:42 -0500

    ready

---
+++ Documents/proyectos-personales/burguer-queen-api/storage.js
@@ -0,0 +1,15 @@
+<script src="https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBbe9fkG9OxZnEue-AJ0gY9Hw3-GhruORg",
+    authDomain: "burger-queen-api-21a53.firebaseapp.com",
+    projectId: "burger-queen-api-21a53",
+    storageBucket: "burger-queen-api-21a53.appspot.com",
+    messagingSenderId: "931661424164",
+    appId: "1:931661424164:web:dcd0dcdb1cfd0b1fdbdab1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
